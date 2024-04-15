7e4d1877bd5dc3bcd9364a6c48e6e012df535a8e
blase147 <solarmails2@gmail.com>
2024-04-15 02:45:00 +0100

    Initial commit

--- app/service/mailchimp_service.rb
+++ app/service/mailchimp_service.rb
@@ -1,27 +1,36 @@
 class MailchimpService
+  API_KEY =  ENV['MAILCHIMP_API_KEY']
+
   def self.subscribe(email, list_id)
     begin
-      Gibbon::Request.lists(list_id).members.create(body: { email_address: email, status: 'subscribed' })
-    rescue Gibbon::MailChimpError => e
-      if e.status_code == 404
-        Rails.logger.error("Mailchimp 404 Error: #{e.raw_body}")
-        raise StandardError.new("Mailchimp list not found. Please check the list ID.")
+      gibbon = Gibbon::Request.new(api_key: API_KEY)
+      list = gibbon.lists(list_id)
+      response = list.members(email).retrieve
+
+      Rails.logger.debug "Response body: #{response.body.inspect}"
+
+      if response.body['status'] == 'subscribed'
+        Rails.logger.debug "Member is already subscribed."
       else
-        raise e
+        Rails.logger.debug "Member is not subscribed. Subscribing now..."
+        list.members(email).upsert(body: { status: 'subscribed' })
       end
+    rescue Gibbon::MailChimpError => e
+      Rails.logger.error "Mailchimp error: #{e.message}"
+      # Handle the error here as needed
     end
   end
 
   def self.subscribed_emails
     list_id = 'a82be852d0' 
     begin
-      response = Gibbon::Request.lists(list_id).members.retrieve(params: { status: 'subscribed' })
+      gibbon = Gibbon::Request.new(api_key: API_KEY)
+      response = gibbon.lists(list_id).members.retrieve(params: { status: 'subscribed' })
       members = response.body['members']
       members.map { |member| member['email_address'] }
     rescue Gibbon::MailChimpError => e
+      Rails.logger.error "Mailchimp error: #{e.message}"
       []
     end
   end
 end
-
-
--- config/initializers/mailchimp.rb
+++ config/initializers/mailchimp.rb
@@ -1,3 +1,3 @@
-Gibbon::Request.api_key = 'befe2ba39cc6ae8ef77908f356fdf747-us10'
+Gibbon::Request.api_key =  ENV['MAILCHIMP_API_KEY']
 Gibbon::Request.timeout = 15
 Gibbon::Request.open_timeout = 15
